b552d9d726874dfc3a559274fe72cafa58571d62
Mojtaba <mojmohammad98@gmail.com>
2024-11-28 11:29:57 +0200

    AS-adding-mail-providers, adding first mail providers with nodemailer and mailtrap.io SMTP

--- aura-strapi/config/plugins.ts
+++ aura-strapi/config/plugins.ts
@@ -6,4 +6,21 @@ export default () => ({
       },
     },
   },
+  email: {
+    config: {
+      provider: "nodemailer",
+      providerOptions: {
+      host: process.env.SMTP_HOST,  // mailtrap.io
+      port: process.env.SMTP_PORT || 587,
+      auth: {
+        user: process.env.SMTP_USERNAME,  // mailtrap.io
+        pass: process.env.SMTP_PASSWORD  // mailtrap.io
+      },
+    },
+    settings: {
+      defaultFrom: 'mojjoj982@gmail.com',  // need to be changed to domain email
+      defaultReplyTo: 'mojjoj982@gmail.com',  // need to be changed to domain email
+    },
+    }
+  }
 });
