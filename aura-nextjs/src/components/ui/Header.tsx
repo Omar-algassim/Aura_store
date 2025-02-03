aa13a0cef32348ff7e367695833eead4274ddb4f
Omar-algassim <omarggg2@gmail.com>
2025-02-04 01:02:39 +0200

    use sueEffect to re render component

--- aura-nextjs/src/components/ui/Header.tsx
+++ aura-nextjs/src/components/ui/Header.tsx
@@ -11,11 +11,18 @@ export function Header() {
   const route = useRouter();
   const cart = useCart();
   const user = useUser();
-  const cart_count = cart?.total_items || 0;
+  const [cartCount, setCartCount] = React.useState(cart?.total_items);
+
+  React.useEffect(() => {
+    setCartCount(cart?.total_items);
+  }, [cart?.total_items]);
+
   return (
     <>
       <div className="w-full bg-white z-30 sticky top-0 flex flex-col items-center justify-center">
         <div className="flex items-center justify-between gap-4 w-[95vw] max-w-[1480px] rounded-2xl py-[20px] tablet:py-[60px] tablet:justify-between flex-wrap bg-white z-30 sticky top-0">
+          
+          {/* nav bar and create account button */}
           <div className="flex justify-between items-center gap-[56px] gap-y-9 order-1">
             <Navbar />
             {user.documentId.length > 0 ? (
@@ -32,6 +39,7 @@ export function Header() {
               </div>
             )}
           </div>
+          
           {/* Search */}
           <div className="w-full hidden tablet:grid order-last tablet:order-2 flex-1 min-w-[320px] h-[78px] laptop:mt-0 ">
             <Search />
@@ -50,11 +58,11 @@ export function Header() {
               />
               <div
                 className={`relative bottom-3 left-11 w-[20px] h-[20px] tablet:w-[24px] tablet:h-[24px] rounded-full text-white text-center text-[12px] tablet-text-[14px] pt-[3px] ${
-                  cart_count === 0 ? "bg-transparent" : "bg-primary"
+                  cartCount === 0 ? "bg-transparent" : "bg-primary"
                 }`}
                 id="cart-icon-tip"
               >
-                {cart_count}
+                {cartCount}
               </div>
             </div>
             <Image
