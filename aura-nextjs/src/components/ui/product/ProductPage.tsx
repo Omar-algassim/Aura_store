6eba550d5785cd73d9069537b65bd1fe7a60dfd5
Omar-algassim <omarggg2@gmail.com>
2025-02-11 14:14:57 +0200

    handle if brand not found

--- aura-nextjs/src/components/ui/product/ProductPage.tsx
+++ aura-nextjs/src/components/ui/product/ProductPage.tsx
@@ -38,6 +38,7 @@ function ProductPageComponent(params: { product: Product }) {
   const [review, setReview] = useState("");
   const [rate, setRate] = useState(0);
 
+  console.log(JSON.stringify(product, null, 2));
   const totalRate = getTotalRate(productReviews);
 
   useEffect(() => {
@@ -295,7 +296,7 @@ function ProductPageComponent(params: { product: Product }) {
               </p>
               <p className="text-[13px] tablet:text-lg font-[400] opacity-85">
                 {" "}
-                {product.brand.name}
+                {product.brand?.name}
               </p>
             </div>
             <div className="w-full flex gap-1">
@@ -403,7 +404,7 @@ function ProductPageComponent(params: { product: Product }) {
               productsType="similar"
               productId={product.documentId}
               categories={product.categories.map((cate) => cate.title)}
-              brand={product.brand.name}
+              brand={product.brand?.name}
             />
           </div>
         </section>
