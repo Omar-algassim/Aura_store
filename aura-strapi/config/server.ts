bfb080f5c90a923b094edcdc245eec3106e93de4
Omar-algassim <omarggg2@gmail.com>
2025-02-11 14:22:08 +0200

    styling

--- aura-strapi/config/server.ts
+++ aura-strapi/config/server.ts
@@ -1,7 +1,7 @@
 export default ({ env }) => ({
-  host: env("HOST", "0.0.0.0"),
+  host: env("HOST", "localhost"),
   port: env.int("PORT", 1337),
-  url: env("URL", "http://0.0.0.0:1337"),
+  url: env("URL", "http://127.0.0.1:1337"),
   app: {
     keys: env.array("APP_KEYS", ["myKeyA", "myKeyB"]),
   },
--- aura-strapi/types/generated/components.d.ts
+++ aura-strapi/types/generated/components.d.ts
@@ -1,10 +1,10 @@
-import type { Schema, Struct } from "@strapi/strapi";
+import type { Schema, Struct } from '@strapi/strapi';
 
 export interface ImagesImages extends Struct.ComponentSchema {
-  collectionName: "components_images_images";
+  collectionName: 'components_images_images';
   info: {
-    description: "";
-    displayName: "images";
+    description: '';
+    displayName: 'images';
   };
   attributes: {
     url: Schema.Attribute.String & Schema.Attribute.Required;
@@ -12,10 +12,10 @@ export interface ImagesImages extends Struct.ComponentSchema {
 }
 
 export interface LocationDeliveryAddress extends Struct.ComponentSchema {
-  collectionName: "components_location_delivery_addresses";
+  collectionName: 'components_location_delivery_addresses';
   info: {
-    displayName: "Delivery Address";
-    icon: "pinMap";
+    displayName: 'Delivery Address';
+    icon: 'pinMap';
   };
   attributes: {
     address: Schema.Attribute.String & Schema.Attribute.Required;
@@ -25,10 +25,10 @@ export interface LocationDeliveryAddress extends Struct.ComponentSchema {
 }
 
 export interface LocationLocation extends Struct.ComponentSchema {
-  collectionName: "components_location_locations";
+  collectionName: 'components_location_locations';
   info: {
-    displayName: "Location";
-    icon: "earth";
+    displayName: 'Location';
+    icon: 'earth';
   };
   attributes: {
     address: Schema.Attribute.String;
@@ -37,12 +37,12 @@ export interface LocationLocation extends Struct.ComponentSchema {
   };
 }
 
-declare module "@strapi/strapi" {
+declare module '@strapi/strapi' {
   export module Public {
     export interface ComponentSchemas {
-      "images.images": ImagesImages;
-      "location.delivery-address": LocationDeliveryAddress;
-      "location.location": LocationLocation;
+      'images.images': ImagesImages;
+      'location.delivery-address': LocationDeliveryAddress;
+      'location.location': LocationLocation;
     }
   }
 }
