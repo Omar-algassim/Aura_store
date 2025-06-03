8711e569b1ed8bb81f24f63413750d34650a60ce
mojtabababiker <mojmohammad98@gmail.com>
2025-06-03 20:57:23 +0300

    fix: update layout styles and improve product page structure

---
+++ aura-nextjs/src/components/icons/ArrowLeft.tsx
@@ -0,0 +1,10 @@
+const ArrowLeftIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" {...props}>
+    <path
+      fill="currentColor"
+      d="M14 17a1 1 0 0 1-.707-.293l-4-4a1 1 0 0 1 0-1.414l4-4a1 1 0 1 1 1.414 1.414L11.414 12l3.293 3.293A1 1 0 0 1 14 17"
+    ></path>
+  </svg>
+);
+
+export default ArrowLeftIcon;
