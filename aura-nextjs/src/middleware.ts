8bb9f51e97417aef2f2e619986c3a8e0096bbfe5
Omar-algassim <omarggg2@gmail.com>
2025-06-03 14:35:29 +0300

    protect dashboard route

--- aura-nextjs/src/middleware.ts
+++ aura-nextjs/src/middleware.ts
@@ -3,7 +3,7 @@ import { cookies } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
 import { User } from "./entities/user-entity";
 
-const protectedRoutes = ["/cart/checkout", "/profile"];
+const protectedRoutes = ["/cart/checkout", "/profile", "/dashboard"];
 const isProtectedRoute = (path: string) => {
   return protectedRoutes.some((route) => path.startsWith(route));
 };
@@ -18,6 +18,21 @@ export async function middleware(request: NextRequest) {
     cookieStore.get("user")?.value || "{}"
   ) as User;
 
+  if (currentPath.startsWith("/dashboard")) {
+    if (user.ok && 'data' in user) {
+    // check if the user is logged as editor
+      if (user.data.role.name !== "editor" && user.data.role.name !== "admin") {
+        // redirect to public dashboard
+        return NextResponse.redirect(
+          new URL("/profile", request.url)
+        );
+      }
+    } else {
+      // if the user is not logged in, redirect to login page
+      cookieStore.set("nextPage", currentPath);
+      return NextResponse.redirect(new URL("/login", request.url));
+    }
+  }
   const message = {
     phone_number: "الرجاء تأكيد رقم الهاتف للمتابعة",
     email: "الرجاء تأكيد البريد الالكتروني للمتابعة",
@@ -33,7 +48,7 @@ export async function middleware(request: NextRequest) {
         return NextResponse.redirect(
           new URL(`/confirm-phone?message=${message.phone_number}`, request.url)
         );
-      } else {
+      } else  {
         return NextResponse.redirect(
           new URL(`/confirm-email?message=${message.email}`, request.url)
         );
