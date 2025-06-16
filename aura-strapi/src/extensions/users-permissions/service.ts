ab0946052dacf6fb85366213dd2beff43d13eae9
Omar-algassim <omarggg2@gmail.com>
2025-06-16 19:48:46 +0300

    edit whatsapp send message to use xpressbot api instead of facebook

--- aura-strapi/src/extensions/users-permissions/service.ts
+++ aura-strapi/src/extensions/users-permissions/service.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import { env } from '@strapi/utils';
 import { errors } from "@strapi/utils";
+import { sep } from 'path';
 
 const { ApplicationError } = errors;
 
@@ -11,83 +12,36 @@ interface requestData {
 
 const WHATSAPP_TOKEN = env('WHATS_ACCESS_TOKEN');
 const WHATSAPP_SENDER = env('SEND_NUMBER');
-const WHATSAPP_URL = `https://graph.facebook.com/v12.0/${WHATSAPP_SENDER}/messages`
+const WHATSAPP_URL = "https://app.xpressbot.org/api/v1/whatsapp/send/template";
 
 function setBod(phone: string, token: string, template: string) {
   const sep_token = `${token.slice(0, 3)}-${token.slice(3, 6)}`;
 
   if (template === "verify_code") {
     return {
-      messaging_product: "whatsapp",
-      to: phone,
-      type: "template",
-      template: {
-        name: template,
-        language: {
-          code: "en_US",
-        },
-        components: [
-          {
-            type: "body",
-            parameters: [
-              {
-                type: "text",
-                text: sep_token,
-              },
-            ],
-          },
-          {
-            type: "button",
-            sub_type: "url",
-            index: "0",
-            parameters: [
-              {
-                type: "text",
-                text: token,
-              },
-            ],
-          },
-        ],
-      },
-    };
+      apiToken: WHATSAPP_TOKEN,
+      phone_number_id: WHATSAPP_SENDER,
+      template_id: "185587",
+      template_header_media_url: "456789",
+      "templateVariable_verifyCode-1": sep_token,
+      "templateVariable-verifyCode-2": token,
+      phone_number: phone,
+      };
   }
   if (template === "reset_password") {
     //TODO: test the template in postman and edit the body
     return {
-      messaging_product: "whatsapp",
-      to: phone,
-      type: "template",
-      template: {
-        name: template,
-        language: {
-          code: "en_US",
-        },
-        components: [
-          {
-            type: "body",
-            parameters: [
-              {
-                type: "text",
-                text: sep_token,
-              },
-            ],
-          },
-          {
-            type: "button",
-            sub_type: "url",
-            index: "0",
-            parameters: [
-              {
-                type: "text",
-                text: token,
-              },
-            ],
-          },
-        ],
-      },
+      apiToken: WHATSAPP_TOKEN,
+      phone_number_id: WHATSAPP_SENDER,
+      template_id: "185588",
+      template_header_media_url: "456789",
+      "templateVariable_resetPassword-1": sep_token,
+      "templateVariable_resetPassword-2": token,
+      phone_number: phone,
     };
   }
 }
+
 function cleanPhone(phone: string) {
   //check if phone number is valid
   if (!phone || phone.length < 10) {
@@ -115,17 +69,12 @@ export default async function sendWhatsappMessage(user: any, data: requestData) 
   //clean the phone number
   const phone_number = cleanPhone(phone);
   
-  //set the header for the request
-  const header = {
-    Authorization: `Bearer ${WHATSAPP_TOKEN}`,
-    "Content-Type": "application/json",
-  };
   //set the body for the request
   const body = setBod(phone_number, token, template); 
 
   try {
     //send the whatsapp message
-    const response = await axios.post(WHATSAPP_URL, body, { headers: header });
+    const response = await axios.post(WHATSAPP_URL, body);
     return response.data;
   } catch (error) {
     throw new ApplicationError("Unable to send whatsapp message due:" + error.message);
