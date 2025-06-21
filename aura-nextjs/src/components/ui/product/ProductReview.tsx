1d220f2b40c881666beffe34d28f72fe843bf455
mojtabababiker <mojmohammad98@gmail.com>
2025-06-21 18:07:38 +0300

    fix: fix querying product reviews bug, and improve some edge cases

--- aura-nextjs/src/components/ui/product/ProductReview.tsx
+++ aura-nextjs/src/components/ui/product/ProductReview.tsx
@@ -1,14 +1,16 @@
-import { Review } from "@/interfaces/dto";
-import cookie from "js-cookie";
-import React from "react";
-import ProductRate from "./ProductRate";
-import { useUser } from "@/components/context";
-import Image from "next/image";
-import { likeFilled, likeOutline } from "@/constants/app-constants";
-import { updateProductReview } from "@/utils/services/products-services";
+import { Review } from '@/interfaces/dto';
+import cookie from 'js-cookie';
+import React from 'react';
+import ProductRate from './ProductRate';
+import { useUser } from '@/components/context';
+import Image from 'next/image';
+import { likeFilled, likeOutline } from '@/constants/app-constants';
+import { updateProductReview } from '@/utils/services/products-services';
+import { useToast } from '@/hooks/use-toast';
 
 function ProductReview({ review }: { review: Review }) {
   const user = useUser();
+  const { toast } = useToast();
   const [likes, setLikes] = React.useState<string[]>(
     review.likes?.map((like) => like.documentId) || []
   );
@@ -18,10 +20,14 @@ function ProductReview({ review }: { review: Review }) {
   const likeReview = async () => {
     // if user is not logged in
     if (!user || !user.documentId) {
-      // ask them to login first
+      toast({
+        title: 'عذراً',
+        description: 'يجب عليك تسجيل الدخول أولاً',
+        variant: 'destructive',
+      });
       return;
     }
-    const jwt = cookie.get("jwt");
+    const jwt = cookie.get('jwt');
     if (!jwt) {
       // ask them to login first
       return;
@@ -49,7 +55,7 @@ function ProductReview({ review }: { review: Review }) {
       return;
     }
     // remove like from the database
-    const jwt = cookie.get("jwt");
+    const jwt = cookie.get('jwt');
     if (!jwt) {
       // ask them to login first
       return;
@@ -68,59 +74,58 @@ function ProductReview({ review }: { review: Review }) {
     }
     setLikes(likes.filter((like) => like !== user.documentId));
   };
+
   return (
-    <div className="flex flex-col py-8 px-4 w-full bg-surface border-2 border-foreground rounded-xl gap-6">
+    <div className='flex flex-col py-8 px-4 w-full bg-surface border-2 border-foreground rounded-xl gap-6'>
       {/* header */}
-      <div className="w-full flex justify-between items-stretch">
+      <div className='w-full flex justify-between items-stretch'>
         {/* username/rate */}
-        <div className="flex flex-col items-center justify-center gap-2">
-          <div className="text-sm font-[500] capitalize">
-            {review.user.username}
+        <div className='flex flex-col items-center justify-center gap-2'>
+          <div className='text-sm font-[500] capitalize'>
+            {review?.user?.username || 'Anonymous'}
           </div>
           <ProductRate starsNumber={review.rate} />
         </div>
 
         {/* date */}
-        <div className="h-full flex items-center justify-center text-xs text-gray-500">
+        <div className='h-full flex items-center justify-center text-xs text-gray-500'>
           {new Date(review.updatedAt).toLocaleDateString()}
         </div>
       </div>
 
       {/* content */}
-      <div className="w-full flex justify-center items-center">
-        <div className="w-full text-right text-sm font-[400] text-[#20202080]">
+      <div className='w-full flex justify-center items-center'>
+        <div className='w-full text-right text-sm font-[400] text-[#20202080]'>
           {review.text}
         </div>
       </div>
 
       {/* footer */}
-      <div className="w-full flex justify-end items-end">
-        <div className="flex items-center justify-center gap-2">
-          <div className="text-sm font-[400] text-[#20202080] flex items-center justify-center">
+      <div className='w-full flex justify-end items-end'>
+        <div className='flex items-center justify-center gap-2'>
+          <div className='text-sm font-[400] text-[#20202080] flex items-center justify-center'>
             {likes.length}
           </div>
           {likes.includes(user.documentId) ? (
             <div
-              className="flex items-center justify-center cursor-default"
-              onClick={unlikeReview}
-            >
+              className='flex items-center justify-center cursor-default'
+              onClick={unlikeReview}>
               <Image
-                className="object-fill object-center"
+                className='object-fill object-center'
                 src={likeFilled}
-                alt="like"
+                alt='like'
                 width={20}
                 height={20}
               />
             </div>
           ) : (
             <div
-              className="flex items-center justify-center cursor-pointer"
-              onClick={likeReview}
-            >
+              className='flex items-center justify-center cursor-pointer'
+              onClick={likeReview}>
               <Image
-                className="object-fill object-center"
+                className='object-fill object-center'
                 src={likeOutline}
-                alt="like"
+                alt='like'
                 width={20}
                 height={20}
               />
