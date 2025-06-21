e74c91f42fea86c361bb001feab3cdcf9f9565df
Omar-algassim <omarggg2@gmail.com>
2025-06-21 15:04:01 +0300

    fix edit mode bad request error

--- aura-nextjs/src/app/dashboard/(admin)/(forms)/new-product/page.tsx
+++ aura-nextjs/src/app/dashboard/(admin)/(forms)/new-product/page.tsx
@@ -22,6 +22,7 @@ import cookie from "js-cookie";
 import React, { useEffect } from "react";
 import { getFieldError } from "@/components/ui/forms/handleError";
 import { useToast } from "@/hooks/use-toast";
+import { Preloader } from "@/components/ui/Preloader";
 
 interface ProductForm {
   editMode?: boolean;
@@ -211,7 +212,6 @@ export default function ProductForm(props: ProductForm) {
   }
 
     const uploadedImages: { url: string; imageId: string }[] = [];
-    console.log("newImages", newImages);
     for (const image of newImages) {
       try {
         const { error, data: response } = await uploadProductImage(image, jwt);
@@ -221,7 +221,7 @@ export default function ProductForm(props: ProductForm) {
           break;
         }
         const resultImageUrl: string = response[0].url;
-        const imageId = response[0].id;
+        const imageId: string = response[0].id.toString();
         if (!resultImageUrl) {
           setError("error uploading images");
           break;
@@ -233,19 +233,20 @@ export default function ProductForm(props: ProductForm) {
         break;
       }
     }
-
+    
     // backtrace storage on error, deleting all the uploaded images
     if (uploadedImages.length !== newImages.length) {
-     return await backtraceStorage(uploadedThumbnail, uploadedImages, jwt);
+      return await backtraceStorage(uploadedThumbnail, uploadedImages, jwt);
     }
-
     // add the images and the thumbnail urls to the product data
-    validation.data.images = uploadedImages;
-
+    validation.data.images =  uploadedImages;
     // create the product
     if (props.editMode) {
       const productId = props.data?.documentId;
-      console.log("productId", props.data?.documentId);
+      console.log("productId", validation.data);
+      if (validation.data.images.length === 0) {
+        delete validation.data.images;
+      }
       if (productId) {
         const editProduct = await updateProduct(
           productId,
@@ -303,7 +304,6 @@ export default function ProductForm(props: ProductForm) {
       if (deleteThumbnail.error) {
         console.log("error deleting thumbnail", deleteThumbnail.error);
       }
-      props.toggleEditMode();
       toast({
         variant: "destructive",
         title: "Error creating",
@@ -317,7 +317,6 @@ export default function ProductForm(props: ProductForm) {
       };
     } else {
       setError(null);
-      props.toggleEditMode();
       toast({
         variant: "success",
         title: "creating success",
@@ -345,6 +344,11 @@ export default function ProductForm(props: ProductForm) {
       dir="ltr"
       className="no-scrollbar relative w-full max-w-screen overflow-y-auto rounded-3xl bg-white p-4 dark:bg-gray-900 lg:p-11"
     >
+      {isPending && (
+        <div className="absolute inset-0 flex items-center justify-center bg-white/50 dark:bg-gray-900/50 z-999">
+          <Preloader />
+        </div>
+      )}
       <div className="px-2 pr-14">
         <h4 className="mb-2 text-2xl font-semibold text-gray-800 dark:text-white/90">
           Fill Product Information
