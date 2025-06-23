dd2525549d38399ccdb8445a58143e6cf1510e35
Omar-algassim <omarggg2@gmail.com>
2025-06-23 17:14:23 +0300

    modify update product type to receive one field update

--- aura-nextjs/src/utils/services/products-services/index.ts
+++ aura-nextjs/src/utils/services/products-services/index.ts
@@ -366,7 +366,7 @@ export const createProduct = async (
 
 export const updateProduct = async (
   id: string,
-  formData: Product,
+  formData: Partial<Product>,
   jwt: string
 ): Promise<{
   message: string;
