1602dd63e03c2e3b4e2be56988b7a1e138d2c15d
mojtabababiker <mojmohammad98@gmail.com>
2025-06-24 19:39:00 +0300

    Update: adding SEO meta tags and OG images

--- aura-nextjs/src/app/products/[productId]/page.tsx
+++ aura-nextjs/src/app/products/[productId]/page.tsx
@@ -1,6 +1,9 @@
-import ProductPageComponent from "@/components/ui/product/ProductPage";
-import { getProduct } from "@/utils/services/products-services";
-import React from "react";
+import React from 'react';
+
+import { getProduct } from '@/utils/services/products-services';
+import { Product } from '@/interfaces/dto';
+
+import ProductPageComponent from '@/components/ui/product/ProductPage';
 
 async function ProductPage({
   params,
@@ -17,4 +20,32 @@ async function ProductPage({
   );
 }
 
+export const generateMetadata = async ({
+  params,
+}: {
+  params: Promise<{ productId: string }>;
+}) => {
+  const { productId } = await params;
+  const { error, data } = await getProduct(productId);
+  if (error) {
+    return {
+      title: 'Aura-Store | Product Not Found',
+      description: 'The product you are looking for does not exist.',
+    };
+  }
+  const product: Product = data.data;
+  return {
+    title: `Aura-Store | ${product.title}`,
+    description: product.description,
+    keywords: [
+      'Personal Care',
+      'Sudan',
+      'Beauty Products',
+      'شامبو',
+      'منتجات العناية الشخصية',
+      'السودان',
+      'منتجات الجمال',
+    ],
+  };
+};
 export default ProductPage;
