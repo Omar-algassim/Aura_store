786df551e99886d55634b7c9319cb03ac52ab36c
Omar-algassim <omarggg2@gmail.com>
2025-08-19 01:34:53 +0300

    fix/ writing in login form number field

--- aura-nextjs/src/components/ui/forms/LoginForm.tsx
+++ aura-nextjs/src/components/ui/forms/LoginForm.tsx
@@ -176,7 +176,7 @@ export function LoginForm({ type = 'phone' }: { type?: 'phone' | 'email' }) {
                   setPhone(value.slice(1));
                   return;
                 }
-                if (value.length > 0 && !/^\d+$/.test(value)) {
+                if (value.length > 0 && !/^\+\d{1,15}$/.test(value)) {
                   setError('الرجاء ادخال رقم هاتف صحيح');
                   return;
                 }
