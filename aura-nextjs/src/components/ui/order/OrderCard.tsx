c1effecc9410f3b0d91debfca842f3b10b183157
Omar-algassim <omarggg2@gmail.com>
2025-08-19 02:30:51 +0300

    add order details in user side

--- aura-nextjs/src/components/ui/order/OrderCard.tsx
+++ aura-nextjs/src/components/ui/order/OrderCard.tsx
@@ -9,6 +9,10 @@ import { BaseUrl } from '@/constants/api-constants';
 import { ButtonPrimary } from '@/components/common/Buttons';
 import { cancelOrder } from '@/utils/services/order-services';
 import { useToast } from '@/hooks/use-toast';
+import { Button } from '../shadcn/button';
+import { Modal } from '../dashboard/ui/modal';
+import Label from '../dashboard/form/Label';
+import OrderItems from '../dashboard/form/order-items';
 
 const STATUS_COLORS = {
   pending: '#FFA500',
@@ -26,6 +30,7 @@ type Props = {
 
 const OrderCard = (props: Props) => {
   const [cardClicked, setCardClicked] = useState(false);
+  const [cartListOpen, setCartListOpen] = useState(false);
   const { toast } = useToast();
 
   const handleOrderCancel = async () => {
@@ -62,6 +67,10 @@ const OrderCard = (props: Props) => {
     }
     setCardClicked(false); // Reset the card clicked state
   };
+
+  function toggleCartListModal() {
+    setCartListOpen((prev) => !prev);
+  }
   return (
     <div
       className={`w-full flex flex-col items-center group gap-8 bg-surface rounded-xl p-5 overflow-hidden ${props.className}`}
@@ -69,7 +78,7 @@ const OrderCard = (props: Props) => {
       {/* order num and order status */}
       <div className='w-full flex justify-between'>
         <p className='text-sm font-[500] capitalize text-[#202020]'>
-          رقم الطلب: {props.order.id}
+          رقم الطلب: {props.order.documentId}
         </p>
         <div className='relative'>
           <p
@@ -79,11 +88,12 @@ const OrderCard = (props: Props) => {
             }}>
             {OrdersPage.orderStatuses[props.order.order_status]}
           </p>
+          
           {/* cancel button */}
           {props.order.order_status === 'pending' ? (
             <div
-              className={clsx(
-                `absolute group-hover:top-0 group-active:top-0 left-0 transition-all duration-300 ease-in-out`,
+            className={clsx(
+              `absolute group-hover:top-0 group-active:top-0 left-0 transition-all duration-300 ease-in-out`,
                 cardClicked ? 'top-0' : '-top-100'
               )}>
               <ButtonPrimary
@@ -95,6 +105,13 @@ const OrderCard = (props: Props) => {
               </ButtonPrimary>
             </div>
           ) : null}
+          <Button
+            variant='link'
+            onClick={toggleCartListModal}
+            className='absolute top-13 right-[-28] text-xs text-primary-dark'
+            >
+            تفاصيل الطلب
+          </Button>
         </div>
       </div>
 
@@ -118,6 +135,25 @@ const OrderCard = (props: Props) => {
           {props.order.total_pay} SDG
         </p>
       </div>
+       <Modal
+        isOpen={cartListOpen}
+        onClose={() => toggleCartListModal()}
+        className='max-w-[700px] max-h-[600px] '>
+        <div className='p-4 mt-18 flex flex-col gap-4 items-start'>
+          <div className='flex gap-6'>
+            <Label>المبلغ:</Label>
+            <p className='ml-6'>{props.order.total_pay} SDG</p>
+          </div>
+          <Label>عناصر الطلب:</Label>
+          {props.order.order_items.map((item) => (
+            <OrderItems
+              key={item.documentId}
+              product={item.product}
+              count={item.count}
+            />
+          ))}
+        </div>
+      </Modal>
     </div>
   );
 };
